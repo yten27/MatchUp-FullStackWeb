6fabcad386c81208d67e67baa9293f42e1108919
yten27 <aytetesh@gmail.com>
2025-11-20 13:02:22 +0100

    grundgerüst bis match-details erstmal

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+#Grundgerüst
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello, World'
+def home():
+    return render_template("base.html")#ähnlich wie main, lädt base.html grundlayout
+
+#Login
+@app.route('/login')
+def login():#logik:
+    return render_template("login.html")
+
+#Registrierung
+@app.route('/register')
+def register():#neuen user hier anlegen:
+    return render_template("register.html")
+
+#Match-Übersicht   
+@app.route('/match')
+def match():#matches anzeigen die in datenbank hinterlegt wurden
+    
+    return render_template("match.html")
+
+#Match-Details
+@app.route('/match_details')
+def match_details():#match details an
+
