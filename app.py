914fe45881030755661d2ca953e78ae277b3c14d
Leon Otte <leon11jr@icloud.com>
2025-12-10 13:15:14 +0100

    Hinzufügen aller Routen

--- app.py
+++ app.py
@@ -20,30 +20,55 @@ def run_insert_sample():
     return "Database flushed and populated with some sample MatchUp data."
 
 
-
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html") #ähnlich wie main, lädt base.html grundlayout
+    return render_template("home.html") #ähnlich wie main, lädt base.html grundlayout, GET
 
 #Login
 @app.route('/login')
-def login():#logik:
+def login(): #logik: GET + POST
     return render_template("login.html")
 
 #Registrierung
 @app.route('/register')
-def register():#neuen user hier anlegen:
+def register():#neuen user hier anlegen: GET + POST
     return render_template("register.html")
 
 #Match-Übersicht   
 @app.route('/allmatches')
-def allmatches():#matches anzeigen die in datenbank hinterlegt wurden
+def allmatches():#matches anzeigen die in datenbank hinterlegt wurden, GET
     
     return render_template("allmatches.html")
 
-#Match-Details
-@app.route('/match_details')
-def match_details():#match details an
+#Match-Details , GET
+@app.route("/matches/<int:match_id>")
+def match_detail(match_id):
+    return render_template("match_detail.html", match_id=match_id)
 
-   return render_template("match_details.html")
+#Create Match, GET + Post 
+@app.route("/matches/create")
+def create_match():
+    return render_template("create_match.html")
+
+#My Matches anzeigen, GET 
+@app.route("/my-matches")
+def my_matches():
+    return render_template("my_matches.html")
+
+#Join Match , Post
+@app.route("/matches/<int:match_id>/join")
+def join_match(match_id):
+    return render_template("match_detail.html", match_id=match_id)
+
+#Leave Match , Post
+@app.route("/matches/<int:match_id>/leave")
+def leave_match(match_id):
+    return render_template("match_detail.html", match_id=match_id)
+
+#Delete Match , Post, NUR HOST
+@app.route("/matches/<int:match_id>/delete")
+def delete_match(match_id):
+    return render_template("match_detail.html", match_id=match_id)
+
+#Nach Aufbau der Seite wird zum Schluss noch die Betragsfunktion, in welcher die Aufteilung des Preises noch geleistet wird
